d28f21639cda45f2688c1824f1a5a5ec02ab78c9
Modupe Akanni <modupe775@gmail.com>
2023-01-05 22:14:14 +0100

    ----

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,29 +7,7 @@ import Timeline from '../components/TimeLine/TimeLine';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
 
-{/* <script type="module"> */}
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyC9viUlHH0Q_zLj4RqVfdFuxYDBEvudEBw",
-    authDomain: "portfolio-d4e90.firebaseapp.com",
-    projectId: "portfolio-d4e90",
-    storageBucket: "portfolio-d4e90.appspot.com",
-    messagingSenderId: "265749788079",
-    appId: "1:265749788079:web:00ad1952f1aca1122f6b5a",
-    measurementId: "G-92T0SYLQYG"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-// </script>
 
 const Home = () => {
   return (
