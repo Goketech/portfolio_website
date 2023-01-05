6e8dc16f7dc8d94c235db05e2e38b33e7b9bc0e8
Modupe Akanni <modupe775@gmail.com>
2023-01-05 22:01:13 +0100

    modified website

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,34 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href='tel:+234-8117266403'>+234-8117266403</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Mail</LinkTitle>
+          <LinkItem href='mailto:modupe775@gmail.com'>modupe775@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Solving Problems one code at a time</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+        <SocialIcons href='https://github.com/goketech'>
+          <AiFillGithub size="3rem" />
+        </SocialIcons>
+        <SocialIcons href='https://www.linkedin.com/in/modupe-akanni-236377235/'>
+          <AiFillLinkedin size="3rem" />
+        </SocialIcons>
+        <SocialIcons href='https://instagram.com/adegoke_akanni'>
+          <AiFillInstagram size="3rem" />
+        </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
